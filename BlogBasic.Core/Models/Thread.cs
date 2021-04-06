3fb0c458d2f55029a6661a872404daf97655bdfb
Its-Ace <68051549+Its-Ace@users.noreply.github.com>
2021-04-06 18:50:36 +0500

    core

---
+++ BlogBasic.Core/Models/Thread.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogBasic.Core.Models
+{
+    public class Thread : Entity
+    {
+            public int Id { get; set; }
+
+            [Display(Name = "Thread Title")]
+            public string ThreadName { get; set; }
+
+            [Display(Name = "Thread Description")]
+            public string ThreadDescription { get; set; }
+
+            [Display(Name = "Thread Content")]
+            public string Content { get; set; }
+
+            public DateTime TimeStamp { get; set; }
+
+            public Thread()
+            {
+                TimeStamp = DateTime.Now;
+            }
+    }
+}
